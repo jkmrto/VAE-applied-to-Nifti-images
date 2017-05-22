37d8a2ecdf4a6c4d57c2e7da938d6c3217152b67
jkmrto <juanka1331@gmail.com>
2017-05-22 17:52:14 +0200

    test

---
+++ recollect_kfolds_results.sh
@@ -0,0 +1,3 @@
+sshpass -p "DircOwz1" scp jcmartinez@150.214.59.226:/mnt/datos/home/jcmartinez/VAN-applied-to-Nifti-images/out/Full\\\ session\\\:\\\ GM\\\ +\\\ WM\\\ with\\\ k\\\ folds/main_out_session* /media/jkmrto/Multimedia/VAN-applied-to-Nifti-images/VAE_kfolds_results
+
+cp  /media/jkmrto/Multimedia/VAN-applied-to-Nifti-images/out/Full\ session\:\ GM\ +\ WM\ with\ k\ folds/main_out_session_* /media/jkmrto/Multimedia/VAN-applied-to-Nifti-images/VAE_kfolds_results/
