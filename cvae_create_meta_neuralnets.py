c594730df23e18ed31c02eccaea64c404f138af2
jkmrto <juanka1331@gmail.com>
2017-07-29 17:33:17 +0200

    test

--- cvae_create_meta_neuralnets.py
+++ cvae_create_meta_neuralnets.py
@@ -18,7 +18,7 @@ explicit_iter_per_region = {
 hyperparams = {'latent_layer_dim': 1000,
                'kernel_size': 5,
                'activation_layer': ops.lrelu,
-               'features_depth': [1, 16, 32],
+               'features_depth': [1, 32, 64],
                'decay_rate': 0.0025,
                'learning_rate': 0.001,
                'lambda_l2_regularization': 0.0001}
