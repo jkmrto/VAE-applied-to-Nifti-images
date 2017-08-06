a23050062dd05e9d370a146750e8d09a65700735
jkmrto <juanka1331@gmail.com>
2017-08-06 12:05:13 +0200

    resulst_reader first version created

---
+++ cvae_create_neural_net_per_class.py
@@ -0,0 +1,47 @@
+import os
+
+import lib.neural_net.kfrans_ops as ops
+import settings
+from lib import session_helper
+from lib.data_loader.pet_loader import load_pet_regions_segmented
+from lib.delete_pre_final_meta_data import delete_simple_session
+from lib.over_regions_lib.cvae_over_regions import \
+    execute_saving_meta_graph_without_any_cv
+
+
+
+# Filtering imates to use base on AD or NOR images
+regions_used = "all"
+list_regions = session_helper.select_regions_to_evaluate(regions_used)
+region_to_img_dict = load_pet_regions_segmented(list_regions,
+                                                bool_logs=False)
+explicit_iter_per_region = {
+    73: 300,
+}
+
+hyperparams = {'latent_layer_dim': 1000,
+               'kernel_size': 5,
+               'activation_layer': ops.lrelu,
+               'features_depth': [1, 64, 128],
+               'decay_rate': 0.0025,
+               'learning_rate': 0.001,
+               'lambda_l2_regularization': 0.0001}
+
+session_conf = {'bool_normalized': True,
+                'n_iters': 1000,
+                "batch_size": 16,
+                "show_error_iter": 10}
+
+path_to_session = execute_saving_meta_graph_without_any_cv(
+    region_cubes_dict=region_to_img_dict,
+    hyperparams=hyperparams,
+    session_conf=session_conf,
+    list_regions=list_regions,
+    path_to_root=settings.path_to_general_out_folder,
+    session_prefix="cvae_create_meta_nets_iter_1000-1000-layer",
+    explicit_iter_per_region=explicit_iter_per_region)
+
+# deleting temporal meta data generated
+session_to_clean_meta_folder = os.path.join(path_to_session, "meta")
+
+delete_simple_session(session_to_
---
+++ results_reader.py
@@ -0,0 +1,73 @@
+import os
+import settings
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from numpy import linspace
+
+
+start = 0.0
+stop = 1.0
+number_of_lines= 1000
+cm_subsection = linspace(start, stop, 9)
+colors = [cm.jet(x) for x in cm_subsection ]
+
+
+out_folder = "CVAE_session_swap_kernel_MRI"
+roc_logs_filename = "roc.logs"
+
+path_to_folder = os.path.join(settings.path_to_general_out_folder,
+                               out_folder)
+
+path_roc_logs = os.path.join(path_to_folder, roc_logs_filename)
+
+roc_log_file = open(path_roc_logs)
+reader = csv.DictReader(roc_log_file, delimiter=";")
+
+keys_used = [' evaluation', 'threshold ', ' false_positive_rate',
+             ' test|train', ' true_positive_rate', 'kernel_size', ' fold']
+
+# false positive rate -> X axis
+# true positive rate -> Y axis
+# kernel size, SWAP term
+
+evaluation_type = ["SVM_weighted", "Complex_Majority_Vote", "Simple_Majority_Vote"]
+execution_type = ["test", "train"]
+kernel_size = [2,3,4,5,6,7,8,9,10]
+fold_selected = 0
+
+list_rows = []
+for row in reader:
+    list_rows.append(row)
+
+
+
+for evaluation in evaluation_type:
+    for execution in execution_type:
+        for kernel in kernel_size:
+            selectec_rows = []
+            for row in list_rows:
+                if row[" evaluation"] == evaluation:
+                    if row[" test|train"] == execution:
+                        if row[" fold"] == str(fold_selected):
+                            plt.figure(int(row["kernel_size"]))
+                            if row["kernel_size"] == str(kernel):
+                                print("selected")
+                                fpr = \
+                                    [float(str_number) for str_number in
+                                     row[" false_positive_rate"].split(",")]
+                                print(fpr)
+                                tpr = \
+                                    [float(str_number) for str_number in
+                                     row[" true_positive_rate"].split(",")]
+                                plt.plot(np.array(fpr), np.array(tpr))
+
+                            img_idi = "{0},{1},{2}".format(
+                                    row[" evaluation"],
+                                    row[" test|train"],
+                                    row[" fold"])
+                            plt.title(img_idi)
+
+                            plt.savefig(os.path.join(path_to_folder,
+                                        "{}.png".format(img_idi)))
