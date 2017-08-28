c9966b7465dc5700ac92da9095eb2ec10bc45c07
jkmrto <juanka1331@gmail.com>
2017-08-28 19:55:29 +0200

    test

--- lib/vae/CVAE_tests.py
+++ lib/vae/CVAE_tests.py
@@ -43,7 +43,7 @@ def auto_execute_with_session_folders():
     hyperparams['features_depth'] = [1, 16, 32]
     hyperparams['image_shape'] = train_images.shape[1:]
     hyperparams['activation_layer'] = ops.lrelu
-    hyperparams['decay_rate'] = 0.002
+    hyperparams['decay_rate'] = 0.0005
     hyperparams['learning_rate'] = 0.0005
     hyperparams['lambda_l2_regularization'] = 0.01
 
