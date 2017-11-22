2693377d0bb8c64a3219d0f5cf5ce6192cac43fa
jkmrto <juanka1331@gmail.com>
2017-11-22 19:50:49 +0100

    test

--- lib/delete_pre_final_meta_data.py
+++ lib/delete_pre_final_meta_data.py
@@ -92,4 +92,4 @@ def delete_simple_session(session_to_clean_meta_folder=None):
 path_to_session = "/home/jkmrto/RemoteDisks/BiosipServer/new_folder_3/VAN-applied-to-Nifti-images/out/cvae_create_meta_nets_iter_1000-layer_22_11_2017_18:40/meta"
 
 #delete_cv_gm_wm_premetada_session()
-delete_simple_session(path_to_session)
+#delete_simple_session(path_to_session)
